211d17ca2c9a86b96127f7992282b4e9829fa3db
seanbenj <seanbenj@gmail.com>
2017-08-30 16:20:12 -0700

    Worked on bobs C# class/code logic challenge.

---
+++ ChallengeSimpleDarts/ChallengeSimpleDarts/Game.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Darts;
+
+namespace ChallengeSimpleDarts
+{
+    public class Game
+    {
+        private Player _Player1;
+        private Player _Player2;
+        private Random _random;
+             
+        public Game(string player1Name, string player2Name)
+        {
+            _Player1 = new Player();
+            _Player1.Name = player1Name;
+
+            _Player2 = new Player();
+            _Player2.Name = player2Name;
+
+            _random = new Random();
+
+        }
+
+        public string Play()
+        {
+            while (_Player1.Score < 300 && _Player2.Score < 300)
+            {
+                playRound(_Player1);
+                playRound(_Player2);
+            }
+            return displayResults();
+        }
+
+        private string displayResults()
+        {
+            string result = string.Format("{0}: {1}<br />{2}: {3}",
+                _Player1.Name,
+                _Player1.Score,
+                _Player2.Name,
+                _Player2.Score);
+
+            return result += "<br/>" + "Winner: " + (_Player1.Score > _Player2.Score ? _Player1.Name : _Player2.Name);
+        }
+
+        private void playRound(Player player)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Dart dart = new Dart(_random);
+                dart.Throw();
+                Score.ScoreDart(player, dart);
+            }
+        }
+    }
+}
---
+++ ChallengeSimpleDarts/Darts/Dart.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Darts
+{
+    
+    public class Dart
+    {
+
+        public int Score { get; set; }
+        public bool IsDouble { get; set; }
+        public bool IsTriple { get; set; }
+
+
+        private Random _Random = new Random();
+
+        public Dart(Random random)
+        {
+            _Random = random; 
+        }
+
+        public void Throw()
+        {
+            Score = _Random.Next(0, 21);
+
+            int multiplier = _Random.Next(1, 101);
+            if (multiplier > 95) IsTriple = true;
+            else if (multiplier > 90) IsDouble = true;
+
+        }
+    }
+}
