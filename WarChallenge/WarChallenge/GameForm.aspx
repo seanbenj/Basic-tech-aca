2263a508c656c08adeead5282724a42199fc1823
seanbenj <seanbenj@gmail.com>
2017-09-05 19:22:16 -0700

    Did Bob's War Challenge

---
+++ WarChallenge/WarChallenge/GameForm.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="GameForm.aspx.cs" Inherits="WarChallenge.GameForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        THE GAME OF WAR STARTS NOW!!<br />
+        <br />
+        <asp:Button ID="playButton" runat="server" OnClick="playButton_Click" Text="PLAY WAR!" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ WarChallenge/WarChallenge/GameForm.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WarChallenge
+{
+    public partial class GameForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        // deck cards 52 total.
+        // 2 players needed.
+        // Split deck Randomly in half, distribute one half to each player. 
+        // Score tally for each player = Total cards.
+        // round = Pull one random card from each player, subtract from each player.
+        // check if carPulled value of player1 is =, <, >, player2.
+        // If cards are = value then intiate WAR!
+        // WAR = pull 2 random cards and don't display in battle, but do display in loot; then
+        // pull 2 more for battle and check if carPulled value of player1 is =, <, >, player2.
+        // WAR Loot = Round(2 cards) + War(4 cards) + ifmuliwar(+ 4 more card for each)
+
+
+        protected void playButton_Click(object sender, EventArgs e)
+        {
+            Game game = new Game("player 1", "player 2");
+            resultLabel.Text = game.Play();
+        }
+    }
+}
