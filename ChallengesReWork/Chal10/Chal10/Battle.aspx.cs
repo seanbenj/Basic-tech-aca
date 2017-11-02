540acc05e9f242dcf370fde9ea0b392b4f6cc4f0
seanbenj <seanbenj@gmail.com>
2017-11-02 10:17:52 -0700

    Refined, added another a class and some more methods.

--- ChallengesReWork/Chal10/Chal10/Battle.aspx.cs
+++ ChallengesReWork/Chal10/Chal10/Battle.aspx.cs
@@ -9,6 +9,18 @@ namespace Chal10
 {
     public partial class Battle : System.Web.UI.Page
     {
+       
+        public class Dice
+        {
+            public int Sides { get; set; }
+            Random random = new Random();
+            public int Roll()
+            {
+                int roll = random.Next(Sides);
+                return roll;
+            }
+        }
+
         public class Character
         {
             public string Name { get; set; }
@@ -16,12 +28,11 @@ namespace Chal10
             public int DamageMax { get; set; }
             public bool AttackBonus { get; set; }
 
-            Random random = new Random();
-            
-            public int Attack()
+            public int Attack(Dice dice)
             {
-                int damage = random.Next(DamageMax);
-                if (AttackBonus) damage += random.Next(5,10);
+                dice.Sides = this.DamageMax;
+                int damage = dice.Roll();
+                if (AttackBonus) damage += dice.Roll() / 5;
                 return damage;
             }
 
@@ -43,6 +54,35 @@ namespace Chal10
             resultLabel.Text += result;
         }
 
+        private void displayWinner(Character character1, Character character2)
+        {
+            if (character2.Health <= 0) resultLabel.Text += "<br><hr/>The hero wins!";
+            else if (character1.Health <= 0) resultLabel.Text += "<br><hr/>The monster wins!";
+            else if (character1.Health <= 0 && character2.Health <= 0) resultLabel.Text += "<br><hr/>They both died!";
+        }
+
+        private void displayBattle(Dice dice, Character character1, Character character2)
+        {
+            int count = 0;
+            
+            do
+            {
+                count++;
+                int damage = character1.Attack(dice);
+                character2.Defend(damage);
+
+                displayRound(damage, character1.Name, character2.Name, character2.Health, count);
+
+                count++;
+                damage = character2.Attack(dice);
+                character1.Defend(damage);
+
+                displayRound(damage, character2.Name, character1.Name, character1.Health, count);
+            } while (character2.Health > 0 && character1.Health > 0);
+
+            displayWinner(character1, character2);
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
             Character hero = new Character()
@@ -61,26 +101,9 @@ namespace Chal10
                 AttackBonus = false
             };
 
-            int count = 0;
+            Dice dice = new Dice();
 
-            while (monster.Health > 0 && hero.Health > 0)
-            {
-                count++;
-                int damage = hero.Attack();
-                monster.Defend(damage);
-
-                displayRound(damage, hero.Name, monster.Name, monster.Health, count);
-
-                count++;
-                damage = monster.Attack();
-                hero.Defend(damage);
-
-                displayRound(damage, monster.Name, hero.Name, hero.Health, count);
-            }
-            if (monster.Health <= 0) resultLabel.Text += "<br><hr/>The hero wins!";
-            else if (hero.Health <= 0) resultLabel.Text += "<br><hr/>The monster wins!";
-            
-                      
+            displayBattle(dice, hero, monster);                    
         }
     }
 }
